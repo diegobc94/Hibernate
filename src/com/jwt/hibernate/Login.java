d7ccd9f0f481a2271d16be2cc63c60e028f79bc0
diego <becerradiego94@gmail.com>
2019-11-30 00:38:47 -0300

    hibernate

---
+++ src/com/jwt/hibernate/Login.java
@@ -0,0 +1,26 @@
+package com.jwt.hibernate;
+
+public class Login {
+
+	private String user;
+	private String password;
+	
+	public Login() {
+		
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
